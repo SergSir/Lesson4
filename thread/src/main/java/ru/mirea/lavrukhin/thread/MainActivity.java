14504406d6303f8d76ed77eedc7368b67e873778
SergSir <126693252+SergSir@users.noreply.github.com>
2023-09-12 15:53:27 +0300

    Update MainActivity.java

--- thread/src/main/java/ru/mirea/lavrukhin/thread/MainActivity.java
+++ thread/src/main/java/ru/mirea/lavrukhin/thread/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         Thread mainThread = Thread.currentThread();
         infoTextView.setText("Имя текущего потока: " + mainThread.getName());
 
-        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 06, НОМЕР ПО СПИСКУ: 18, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: ГОРБАТАЯ ГОРА");
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: 06, НОМЕР ПО СПИСКУ: 14, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: ДОКТОР СТРЭНДЖ");
         infoTextView.append("\n Новое имя потока: " + mainThread.getName());
         Log.d(MainActivity.class.getSimpleName(), "Stack: " + Arrays.toString(mainThread.getStackTrace()));
 
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
                 new	Thread(new	Runnable()	{
                     public	void run()	{
                         int	numberThread = counter++;
-                        Log.d("ThreadProject",	String.format("Запущен	поток:	№%d / Cтудентом	группы:	%s /	Номер	по списку:	%d	",	numberThread,	"БСБО-06-21",	18));
+                        Log.d("ThreadProject",	String.format("Запущен	поток:	№%d / Cтудентом	группы:	%s /	Номер	по списку:	%d	",	numberThread,	"БСБО-06-21",	14));
                         long	endTime	=	System.currentTimeMillis()	+	20	*	1000;
                         if(String.valueOf(binding.editText1.getText()).isEmpty()) {
                             throw new IllegalArgumentException("String is empty");
